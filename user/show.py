2b6017fe30f7547fca080fa9570a7b4bfadbf4b2
wuyouliang <wuyouliang@momenta.ai>
2020-09-10 20:26:00 +0800

    add average_line

--- user/show.py
+++ user/show.py
@@ -29,6 +29,8 @@ class Show(object):
         self.data = csv_data.values.tolist()
         self.freq = freq
 
+        self.colors = {'ma5':'gold', 'ma10':'pink', 'ma20': 'blueviolet', 'ma60':'cyan'}
+
     def signal_handler(self, signal, frame):
         sys.exit(0)
 
@@ -40,6 +42,7 @@ class Show(object):
         xs = [datetime.strptime(str(d)[0:-2], '%Y%m%d').date() for d in x]
         # print(xs)
         y = [i[1] for i in self.data]
+        y = [round(i,2) for i in y]
         y.reverse()
         # print(y)
         return xs, y
@@ -83,20 +86,19 @@ class Show(object):
     def draw_point(self, high_x, high_y, low_x, low_y):
         # 绘制散点(3, 6)
         for i in range(len(high_y)):
-            plt.scatter(high_x[i], high_y[i], s=30, color='red')      # s 为点的 size
-            # 对(3, 6)做标注
+            plt.scatter(high_x[i], high_y[i], s=25, color='red')      # s 为点的 size
             plt.annotate(str(high_y[i]), xy=(high_x[i], high_y[i]+0.5), fontsize=10, xycoords='data')      # 在(3.3, 5.5)上做标注
 
         # 绘制散点(3, 6)
         for i in range(len(low_y)):
-            plt.scatter(low_x[i], low_y[i], s=30, color='green')      # s 为点的 size
-            # 对(3, 6)做标注
+            plt.scatter(low_x[i], low_y[i], s=25, color='green')      # s 为点的 size
             plt.annotate(str(low_y[i]), xy=(low_x[i], low_y[i]-3.5), fontsize=10, xycoords='data')      # 在(3.3, 5.5)上做标注
 
         # plt.text(3.3, 5, "this point very important",
         #     fontdict={'size': 12, 'color': 'green'})  # xycoords='data' 是说基于数据的值来选位置
 
     def draw_high_line(self, high_x, high_y):
+        plt.plot(high_x, high_y, color='red', linewidth=1.0, linestyle="--", label="y")
         x = high_x
         y = high_y
         linewidth = 1.0
@@ -104,10 +106,11 @@ class Show(object):
             high_x, high_y, temp_x, temp_y = self.get_point(x, y)
             x = high_x
             y = high_y
-            linewidth += 0.5
+            linewidth += 0.75
             plt.plot(x, y, color='red', linewidth=linewidth, linestyle="--", label="y")
 
     def draw_low_line(self, low_x, low_y):
+        plt.plot(low_x, low_y, color='green', linewidth=1.0, linestyle="--", label="y")
         x = low_x
         y = low_y
         linewidth = 1.0
@@ -115,35 +118,101 @@ class Show(object):
             temp_x, temp_y, low_x, low_y = self.get_point(x, y)
             x = low_x
             y = low_y
-            linewidth += 0.5
+            linewidth += 0.75
             plt.plot(x, y, color='green', linewidth=linewidth, linestyle="--", label="y")
+
+    def get_average(self, price, number):
+        average = []
+        index = 0
+        for i in range(len(price)):
+            if i < number:
+                index = 0
+            else :
+                index = i-(number-1)
+            p = price[index:i+1]
+            average.append(np.mean(p))
+        return average
+
+    def average_line(self, xs, ys):
+        ma5 = self.get_average(ys, 4)
+        ma10 = self.get_average(ys, 9)
+        ma20 = self.get_average(ys, 18)
+        ma60 = self.get_average(ys, 60)
+
+        pre_rush = False
+        rush = False
+        pre_run = False
+        run = False
+        for i in range(20, len(ys)):
+            # rush
+            if ma5[i] >= ma10[i]:
+                if pre_rush == False:
+                    pre_rush = True
+                    code = self.name + ':'
+                    print(code, xs[i], 'pre_rush!')
+                if ma10[i] >= ma20[i]:
+                    if rush == False:
+                        rush = True
+                        code = self.name + ':'
+                        plt.scatter(xs[i], ys[i], s=50, color='red')      # s 为点的 size
+                        print(code, xs[i], 'rush!!!')
+
+            if ma10[i] < ma20[i]:
+                rush = False
+            if ma5[i] < ma10[i]:
+                if rush == False:
+                    pre_rush = False
+
+            # run
+            if ma5[i] <= ma10[i]:
+                if pre_run == False:
+                    pre_run = True
+                    code = self.name + ':'
+                    print(code, xs[i], 'pre_run!')
+                if ma10[i] <= ma20[i]:
+                    if run == False:
+                        run = True
+                        code = self.name + ':'
+                        plt.scatter(xs[i], ys[i], s=50, color='green')      # s 为点的 size
+                        print(code, xs[i], 'run!!!')
+
+            if ma10[i] > ma20[i]:
+                run = False
+            if ma5[i] > ma10[i]:
+                if run == False:
+                    pre_run = False
+
+        plt.plot(xs, ma5, color=self.colors['ma5'], linewidth=1.5, linestyle="-", label='ma5')
+        plt.plot(xs, ma10, color=self.colors['ma10'], linewidth=1.5, linestyle="-", label='ma10')
+        plt.plot(xs, ma20, color=self.colors['ma20'], linewidth=1.5, linestyle="-", label='ma20')
+        # plt.plot(xs, ma60, color=self.colors['ma60'], linewidth=1.5, linestyle="-", label='ma60')
+
     def show(self):
         # 创建一个点数为 8 x 6 的窗口, 并设置分辨率为 80像素/每英寸
         # plt.figure(figsize=(16, 9), dpi=80)
         # 再创建一个规格为 1 x 1 的子图
-        # plt.subplot(111)
-        fig1, ax = plt.subplots()
+        plt.subplot(111)
+        # fig1, ax = plt.subplots()
         plt.title(self.name)
 
         xs, ys = self.get_position()
+
+        self.average_line(xs, ys)
+
         high_x, high_y, low_x, low_y = self.get_point(xs, ys)
         self.draw_point(high_x, high_y, low_x, low_y)
-        self.draw_high_line(high_x, high_y)
-        self.draw_low_line(low_x, low_y)
+        # self.draw_high_line(high_x, high_y)
+        # self.draw_low_line(low_x, low_y)
 
-        plt.plot(high_x, high_y, color='red', linewidth=1.0, linestyle="--", label="y")
-        plt.plot(low_x, low_y, color='green', linewidth=1.0, linestyle="--", label="y")
-        # 绘制颜色为蓝色、宽度为 1 像素的连续曲线 y1
-        plt.plot(xs, ys, 'b', linewidth=1.0, linestyle="-", label="y")
-        # plt.gcf().autofmt_xdate()
+        plt.plot(xs, ys, color='blue', linewidth=1.0, linestyle="-", label="price")
+        plt.legend(loc='upper left',ncol=2)   # 图例
 
-        # plt.legend(loc="upper left")
         # 设置横轴的上下限
         # plt.xlim(20160818, 20200901)
         # 设置纵轴的上下限
         # plt.ylim(30, 500)
         # 设置横轴标签
-        # plt.xlabel("X")
+        plt.xlabel("X")
         # 设置纵轴标签
         # plt.ylabel("Y")
         # 设置横轴精准刻度
